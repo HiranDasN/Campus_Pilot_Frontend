80ab166c7b98e5638381b5e020dbc4904ae5c39f
HiranDasN <hirannanooth@gmail.com>
2024-02-17 22:49:55 +0530

    v5

--- src/components/StudentComponent/Delete_Student_Acc.jsx
+++ src/components/StudentComponent/Delete_Student_Acc.jsx
@@ -1,9 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { deleteStudentAPI} from '../../services/allAPI';
-import Swal from 'sweetalert2';
 import { studLogEditContext } from '../../context/ContextShare';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -27,50 +24,7 @@ function Delete_Student_Acc() {
     setStudentLoginInfo(existingStudent);
   }, [studLogEdit]);
 
-  const navigate = useNavigate()
 
-  const handleDeleteLogout = ()=>{
-    sessionStorage.removeItem("token")
-    sessionStorage.removeItem("existingStudent")
-
-
-    navigate('/')
-  }
-
-  const handleDelete = async(id)=>{
-    const isConfirmed = await Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!',
-    });
-
-    if (isConfirmed.isConfirmed){  
-
-    const token = sessionStorage.getItem("token")
-    const reqHeader = {
-      "Content-Type" : "application/json",
-      "Authorization" : `Bearer ${token}`
-    }
-    const result = await deleteStudentAPI(id,reqHeader)
-    console.log(result);
-    if(result.status === 200){
-      Swal.fire({
-        title:"Success" ,
-        text:`Account Deleted Successfully`,
-        icon: "success"
-      });
-      handleDeleteLogout();
-
-    }
-    else{
-      console.log(result.response.data);
-    }
-  }
-  }
   return (
     <Container className="my-5">
     <div data-aos="fade-up-left" className="card cardshd p-4 rounded">
@@ -96,9 +50,7 @@ function Delete_Student_Acc() {
           </div>
         </Col>
       </Row>
-      <div className="d-flex justify-content-center mt-4">
-        <Button onClick={()=>handleDelete(StudentLoginInfo._id)}  variant="danger">Delete Account</Button>
-      </div>
+     
     </div>
   </Container>
   )
